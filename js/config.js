b32d45df86c01134180096fa25bf4175e09fbc6c
albertograziotto-sys <alberto.graziotto@gmail.com>
2026-08-06 10:02:08 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+export const SUPABASE_URL = 'https://tmaxqbosibkxrghgwfzi.supabase.co';
+export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtYXhxYm9zaWJreHJnaGd3ZnppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzODA2MjUsImV4cCI6MjA5OTk1NjYyNX0.xMVQd7yHyUuoCyH1JajJttYRNR5qhEy_W6TsMcDgJA0';
+
+export const clientDB = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
